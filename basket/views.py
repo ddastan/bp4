c3d0736f8832afa2a3978c725e3eb257ea147b4d
ddastan <dm_temir@icloud.com>
2024-11-17 16:26:40 +0600

    hw5

--- basket/views.py
+++ basket/views.py
@@ -1,10 +1,20 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Order
 from .forms import OrderForm
 
 def order_list(request):
-    orders = Order.objects.all()
-    return render(request, 'order_list.html', {'orders': orders})
+    query = request.GET.get('q')  # Получаем значение из строки поиска
+    if query:
+        orders = Order.objects.filter(
+            Q(name__icontains=query) |
+            Q(phone_number__icontains=query) |
+            Q(email__icontains=query) |
+            Q(book__title__icontains=query)
+        )
+    else:
+        orders = Order.objects.all()
+    return render(request, 'order_list.html', {'orders': orders, 'query': query})
 
 def order_create(request):
     if request.method == 'POST':
